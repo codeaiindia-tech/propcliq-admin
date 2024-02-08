a1bccaaf0da442a0c7f9d2eed2cbb6f959f553af
root <root@ip-172-31-80-255.ec2.internal>
2024-02-08 04:25:44 +0000

    remove download folder

--- Downloads/metronic_react_v8.2.3_demo2 2/metronic_react_v8.2.3_demo2/src/app/pages/AddProperty/addProperty.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Vertical } from '../../modules/wizards/components/Vertical';
-const AddProperty = () => {
-  return <Vertical />;
-};
-export default AddProperty;
