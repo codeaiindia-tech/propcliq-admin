9bbb0a06e54984d442c71fb21f4f511b728bf89e
codeaiindia-tech <codeaiindia@gmail.com>
2026-03-12 17:41:12 +0530

    updated login route

--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -4,20 +4,26 @@ import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 import { useFormik } from 'formik';
 import { useAuth } from '../core/Auth';
-import {sendOtp, sendOtpToPhone} from '../../../Apis/AuthApiList'
-import Verify from  './VerifyOtp';
-
+import { sendOtp, sendOtpToPhone } from '../../../Apis/AuthApiList';
+import Verify from './VerifyOtp';
 
 const loginSchema = Yup.object().shape({
-    email: Yup.string().email('Wrong email format').min(3, 'Minimum 3 symbols').max(50, 'Maximum 50 symbols').required('Email is required'),
-    password: Yup.string().min(3, 'Minimum 3 symbols').max(50, 'Maximum 50 symbols').required('Password is required'),
+  email: Yup.string()
+    .email('Wrong email format')
+    .min(3, 'Minimum 3 symbols')
+    .max(50, 'Maximum 50 symbols')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(3, 'Minimum 3 symbols')
+    .max(50, 'Maximum 50 symbols')
+    .required('Password is required'),
 });
 
 const initialValues = {
-    email: '',
-    password: '',
-    // email: 'Sonalsawarn00@gmail.com',
-    // password: 'Sonal123',
+  email: '',
+  password: '',
+  // email: 'Sonalsawarn00@gmail.com',
+  // password: 'Sonal123',
 };
 
 /*
@@ -27,248 +33,292 @@ const initialValues = {
 */
 
 export function Login() {
-    const [loading, setLoading] = useState(false);
-    const [signInViaPhone, setSignInViaPhone] = useState(true);
-    const { saveAuth, setCurrentUser } = useAuth();
-    const [openOtpFlag, setOpenOtpFlag] = useState(false);
-    const [ userData, setUserData] =  useState<any>();
-    const [errorFlag, setErrorFlag] = useState(false);
-    const [errorFlagMsg, setErrorFlagMsg] = useState('');
-    const [phoneNo, setPhoneNo] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [signInViaPhone, setSignInViaPhone] = useState(true);
+  const { saveAuth, setCurrentUser } = useAuth();
+  const [openOtpFlag, setOpenOtpFlag] = useState(false);
+  const [userData, setUserData] = useState<any>();
+  const [errorFlag, setErrorFlag] = useState(false);
+  const [errorFlagMsg, setErrorFlagMsg] = useState('');
+  const [phoneNo, setPhoneNo] = useState('');
 
-    const validatePhoneNo = (phoneNo: any) => {
-        if(phoneNo !== '') {
-            setErrorFlag(false)
-            setErrorFlagMsg('')
-            return true
+  const validatePhoneNo = (phoneNo: string) => {
+    const cleanedPhoneNo = phoneNo.trim();
+    const phoneRegex = /^[0-9]{10}$/;
+
+    if (phoneRegex.test(cleanedPhoneNo)) {
+      setErrorFlag(false);
+      setErrorFlagMsg('');
+      return true;
+    } else {
+      setErrorFlag(true);
+      setErrorFlagMsg('Enter Valid 10 Digit Phone Number');
+      return false;
+    }
+  };
+
+  const sendOtpToPhoneNo = async () => {
+    console.log(phoneNo);
+
+    if (!validatePhoneNo(phoneNo)) {
+      setLoading(false);
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const sendOtpToMail = await sendOtpToPhone({ phone: phoneNo });
+      console.log('sendOtpToMail', sendOtpToMail);
+
+      if (sendOtpToMail?.success === true) {
+        setErrorFlag(false);
+        setErrorFlagMsg('');
+        setOpenOtpFlag(true);
+        setUserData({ phoneNo: phoneNo });
+      } else {
+        setErrorFlag(true);
+        setErrorFlagMsg(sendOtpToMail.message);
+      }
+    } catch (error) {
+      console.error(error);
+      saveAuth(undefined);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: loginSchema,
+    onSubmit: async (values, { setStatus, setSubmitting }) => {
+      setLoading(true);
+      try {
+        const sendOtpToMail = await sendOtp({
+          email: values.email,
+          password: values.password,
+        });
+
+        if (sendOtpToMail?.success === true) {
+          setErrorFlag(false);
+          setErrorFlagMsg('');
+          setOpenOtpFlag(true);
+          setUserData(values);
         } else {
-            setErrorFlag(true)
-            setErrorFlagMsg('Enter Valid Phone Number')
-            return false;
+          setErrorFlag(true);
+          setErrorFlagMsg(sendOtpToMail.message);
         }
-        
-    }
-    
+      } catch (error) {
+        console.error(error);
+        saveAuth(undefined);
+        setStatus('The login details are incorrect');
+        setSubmitting(false);
+      } finally {
+        setLoading(false);
+      }
+    },
+  });
 
-    const sendOtpToPhoneNo  = async() => {
-        console.log(phoneNo)
-        setLoading(true);
-        try {
-           if (validatePhoneNo(phoneNo) === true) {
-            const sendOtpToMail = await sendOtpToPhone({"phone" : phoneNo});
-            console.log('sendOtpToMail',sendOtpToMail)
-   
-            if(sendOtpToMail?.success === true) {
-                setLoading(false);
-                setOpenOtpFlag(true);
-                setUserData({'phoneNo': phoneNo });
-            }else {
-                setErrorFlag(true)
-                setErrorFlagMsg(sendOtpToMail.message)
-                setLoading(false);
-            }
-          
-           } 
-            
-        } catch (error) {
-            console.error(error);
-            saveAuth(undefined);
-            setLoading(false);
-        }  
-    }
-    
+  const backToPage = () => {
+    setOpenOtpFlag(!openOtpFlag);
+    setPhoneNo('');
+  };
 
-    const formik = useFormik({
-        initialValues,
-        validationSchema: loginSchema,
-        onSubmit: async (values, { setStatus, setSubmitting }) => {
-            setLoading(true);
-            try {
-                const sendOtpToMail = await sendOtp({"email" : values.email, "password" : values.password});
-       
-                if(sendOtpToMail?.success === true) {
-                    setLoading(false);
-                    setOpenOtpFlag(true);
-                    setUserData(values);
-                }else {
-                    setErrorFlag(true)
-                    setErrorFlagMsg(sendOtpToMail.message)
-                    setLoading(false);
-                }
-              
-            } catch (error) {
-                console.error(error);
-                saveAuth(undefined);
-                setStatus('The login details are incorrect');
-                setSubmitting(false);
-                setLoading(false);
-            }
-        },
-    });
-const backToPage= () => {
-    setOpenOtpFlag(!openOtpFlag)
-    setPhoneNo('')
-}
-   
+  return (
+    <>
+      {openOtpFlag && (
+        <Verify
+          backToSignIn={backToPage}
+          signInViaPhone={signInViaPhone}
+          userData={userData}
+        ></Verify>
+      )}
 
-    return (
-        <>
-        {openOtpFlag && (
-            // <OTPApp></OTPApp>
-            <Verify backToSignIn = {backToPage} signInViaPhone={signInViaPhone} userData={userData}></Verify>
-        )}
-       {!openOtpFlag && ( <form className="form w-100" onSubmit={formik.handleSubmit} noValidate id="kt_login_signin_form">
-            {/* begin::Heading */}
-            <div className="text-center mb-11">
-                <h1 className="text-gray-900 fw-bolder mb-3">Sign In</h1>
-                <div className="text-gray-500 fw-semibold fs-6">Sign in with this account across the following sites.</div>
+      {!openOtpFlag && (
+        <form
+          className="form w-100"
+          onSubmit={formik.handleSubmit}
+          noValidate
+          id="kt_login_signin_form"
+        >
+          {/* begin::Heading */}
+          <div className="text-center mb-11">
+            <h1 className="text-gray-900 fw-bolder mb-3">Sign In</h1>
+            <div className="text-gray-500 fw-semibold fs-6">
+              Sign in with this account across the following sites.
             </div>
-            {/* begin::Heading */}
-
-            {/* begin::Login options */}
-            <div className='row g-3 mb-9' onClick={() => setSignInViaPhone(!signInViaPhone)}>
-         <a
-            href='#'
-            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
-          >
-           
-           {signInViaPhone   ?   'Sign in with Email and Password' : 'Sign in Via Phone'}
-          </a>
           </div>
-            {/* end::Login options */}
+          {/* end::Heading */}
 
-            {/* begin::Separator */}
-            {(formik.status || errorFlag) && (
-                <div className="mb-lg-15 alert alert-danger">
-                    <div className="alert-text font-weight-bold">{formik.status ? formik.status : errorFlagMsg}</div>
-                </div>
-            )}
-            
-            {/* end::Separator */}
-            {signInViaPhone && (
-                  <div className="fv-row mb-8">
-                  <label className="form-label fs-6 fw-bolder text-gray-900">Phone Number</label>
-                  <input
-                      placeholder="Enter Phone No "
-                      onChange={e => setPhoneNo(e.target.value)}
-                    
-                      className={clsx(
-                          'form-control bg-transparent',   
-                      )}
-                      type="phone"
-                      name="phone"
-                      autoComplete="off"
-                  />
-              <div className="fv-row mb-8" style ={{marginTop:'3%'}}>
-                   <button
-                       type="submit"
-                       id="kt_sign_up_submit"
-                       className="btn btn-lg btn-primary w-100 mb-5"
-                       onClick = {sendOtpToPhoneNo}
-                     
-                   >
-                       <span className="indicator-label">Continue</span>
-                   </button>
-                   </div>
-                   </div>
-                   
-                   
+          {/* begin::Login options */}
+          <div
+            className="row g-3 mb-9"
+            onClick={() => setSignInViaPhone(!signInViaPhone)}
+          >
+            <button
+              type="button"
+              className="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100"
+            >
+              {signInViaPhone
+                ? 'Sign in with Email and Password'
+                : 'Sign in Via Phone'}
+            </button>
+          </div>
+          {/* end::Login options */}
 
-            )
-           
-            }
+          {/* begin::Separator */}
+          {(formik.status || errorFlag) && (
+            <div className="mb-lg-15 alert alert-danger">
+              <div className="alert-text font-weight-bold">
+                {formik.status ? formik.status : errorFlagMsg}
+              </div>
+            </div>
+          )}
+          {/* end::Separator */}
 
-           {!signInViaPhone && (
-           <div>
-
-            {/* begin::Form group */}
+          {signInViaPhone && (
             <div className="fv-row mb-8">
-                <label className="form-label fs-6 fw-bolder text-gray-900">Email</label>
+              <label className="form-label fs-6 fw-bolder text-gray-900">
+                Phone Number
+              </label>
+              <input
+                placeholder="Enter Phone No"
+                onChange={(e) => setPhoneNo(e.target.value)}
+                value={phoneNo}
+                className={clsx('form-control bg-transparent')}
+                type="tel"
+                name="phone"
+                autoComplete="off"
+              />
+
+              <div className="fv-row mb-8" style={{ marginTop: '3%' }}>
+                <button
+                  type="button"
+                  id="kt_sign_up_submit"
+                  className="btn btn-lg btn-primary w-100 mb-5"
+                  onClick={sendOtpToPhoneNo}
+                  disabled={loading}
+                >
+                  {!loading && <span className="indicator-label">Continue</span>}
+                  {loading && (
+                    <span
+                      className="indicator-progress"
+                      style={{ display: 'block' }}
+                    >
+                      Please wait...
+                      <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+                    </span>
+                  )}
+                </button>
+              </div>
+            </div>
+          )}
+
+          {!signInViaPhone && (
+            <div>
+              {/* begin::Form group */}
+              <div className="fv-row mb-8">
+                <label className="form-label fs-6 fw-bolder text-gray-900">
+                  Email
+                </label>
                 <input
-                    placeholder="Email"
-                    {...formik.getFieldProps('email')}
-                    className={clsx(
-                        'form-control bg-transparent',
-                        { 'is-invalid': formik.touched.email && formik.errors.email },
-                        {
-                            'is-valid': formik.touched.email && !formik.errors.email,
-                        }
-                    )}
-                    type="email"
-                    name="email"
-                    autoComplete="off"
+                  placeholder="Email"
+                  {...formik.getFieldProps('email')}
+                  className={clsx(
+                    'form-control bg-transparent',
+                    { 'is-invalid': formik.touched.email && formik.errors.email },
+                    {
+                      'is-valid':
+                        formik.touched.email && !formik.errors.email,
+                    }
+                  )}
+                  type="email"
+                  name="email"
+                  autoComplete="off"
                 />
                 {formik.touched.email && formik.errors.email && (
-                    <div className="fv-plugins-message-container">
-                        <span role="alert">{formik.errors.email}</span>
-                    </div>
+                  <div className="fv-plugins-message-container">
+                    <span role="alert">{formik.errors.email}</span>
+                  </div>
                 )}
-            </div>
-            {/* end::Form group */}
+              </div>
+              {/* end::Form group */}
 
-            {/* begin::Form group */}
-            <div className="fv-row mb-3">
-                <label className="form-label fw-bolder text-gray-900 fs-6 mb-0">Password</label>
+              {/* begin::Form group */}
+              <div className="fv-row mb-3">
+                <label className="form-label fw-bolder text-gray-900 fs-6 mb-0">
+                  Password
+                </label>
                 <input
-                    type="password"
-                    autoComplete="off"
-                    {...formik.getFieldProps('password')}
-                    className={clsx(
-                        'form-control bg-transparent',
-                        {
-                            'is-invalid': formik.touched.password && formik.errors.password,
-                        },
-                        {
-                            'is-valid': formik.touched.password && !formik.errors.password,
-                        }
-                    )}
+                  type="password"
+                  autoComplete="off"
+                  {...formik.getFieldProps('password')}
+                  className={clsx(
+                    'form-control bg-transparent',
+                    {
+                      'is-invalid':
+                        formik.touched.password && formik.errors.password,
+                    },
+                    {
+                      'is-valid':
+                        formik.touched.password && !formik.errors.password,
+                    }
+                  )}
                 />
                 {formik.touched.password && formik.errors.password && (
-                    <div className="fv-plugins-message-container">
-                        <div className="fv-help-block">
-                            <span role="alert">{formik.errors.password}</span>
-                        </div>
+                  <div className="fv-plugins-message-container">
+                    <div className="fv-help-block">
+                      <span role="alert">{formik.errors.password}</span>
                     </div>
+                  </div>
                 )}
-            </div>
-            {/* end::Form group */}
+              </div>
+              {/* end::Form group */}
 
-            {/* begin::Wrapper */}
-            <div className="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+              {/* begin::Wrapper */}
+              <div className="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                 <div />
 
                 {/* begin::Link */}
                 <Link to="/auth/forgot-password" className="link-primary">
-                    Forgot Password ?
+                  Forgot Password ?
                 </Link>
                 {/* end::Link */}
-            </div>
-            {/* end::Wrapper */}
+              </div>
+              {/* end::Wrapper */}
 
-            {/* begin::Action */}
-            <div className="d-grid mb-10">
-                <button type="submit" id="kt_sign_in_submit" className="btn btn-primary" disabled={formik.isSubmitting || !formik.isValid}>
-                    {!loading && <span className="indicator-label">Continue</span>}
-                    {loading && (
-                        <span className="indicator-progress" style={{ display: 'block' }}>
-                            Please wait...
-                            <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
-                        </span>
-                    )}
+              {/* begin::Action */}
+              <div className="d-grid mb-10">
+                <button
+                  type="submit"
+                  id="kt_sign_in_submit"
+                  className="btn btn-primary"
+                  disabled={formik.isSubmitting || !formik.isValid || loading}
+                >
+                  {!loading && <span className="indicator-label">Continue</span>}
+                  {loading && (
+                    <span
+                      className="indicator-progress"
+                      style={{ display: 'block' }}
+                    >
+                      Please wait...
+                      <span className="spinner-border spinner-border-sm align-middle ms-2"></span>
+                    </span>
+                  )}
                 </button>
+              </div>
+              {/* end::Action */}
             </div>
-            {/* end::Action */}
-            </div> )}
-            <div className="text-gray-500 text-center fw-semibold fs-6">
-                Not a Member yet?{' '}
-                <Link to="/auth/registration" className="link-primary">
-                    Sign up
-                </Link>
-            </div>
-        </form>)}
-        
+          )}
 
-
-        </>
-    );
+          <div className="text-gray-500 text-center fw-semibold fs-6">
+            Not a Member yet?{' '}
+            <Link to="/auth/registration" className="link-primary">
+              Sign up
+            </Link>
+          </div>
+        </form>
+      )}
+    </>
+  );
 }
